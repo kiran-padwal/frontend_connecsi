55e141ad5fb06279a21f28c1d2390b937751db8e
kiranPadwal <padwalkiran1985@gmail.com>
2019-06-20 14:40:38 +0200

    bugs offer module

--- static/app-assets/js/custom.js
+++ static/app-assets/js/custom.js
@@ -1476,7 +1476,7 @@ $(document).ready(function () {
                         '<div class="offers_item__footer">' +
                         '<div class="row d-flex align-items-center mt-2">' +
                         ' <div class="col-md-6 d-flex flex-row align-items-center">' +
-                        (value.channel_img != "" ? '<img src="/static/img/'+value.channel_img+'" class="rounded offers_item__profile-thumb d-block" alt="Card image">' : '<img src="/static/img/'+fix_img+'" class="rounded offers_item__profile-thumb d-block" alt="Card image">') +
+                        (value.channel_img != "" ? '<img src="'+value.channel_img+'" class="rounded offers_item__profile-thumb d-block" alt="Card image">' : '<img src="/static/img/'+fix_img+'" class="rounded offers_item__profile-thumb d-block" alt="Card image">') +
                         '<span>' + value.first_name + ' ' + value.last_name + '</span>' +
                         '</div>' +
                         ' <div class="col-md-6 text-right">' +
