78ed9761d98c9722f3b87b1e9cbc00b5de609f3f
kiranPadwal <padwalkiran1985@gmail.com>
2019-09-05 13:34:16 +0200

    subscription update

--- static/app-assets/js/script.js
+++ static/app-assets/js/script.js
@@ -2,7 +2,41 @@
  * Created by ashishtyagi9622 on 17/6/19.
  */
 
+function abbrNum(number, decPlaces) {
+    // 2 decimal places => 100, 3 => 1000, etc
+    decPlaces = Math.pow(10,decPlaces);
 
+    // Enumerate number abbreviations
+    var abbrev = [ " K", " M", " B", " T" ];
+
+    // Go through the array backwards, so we do the largest first
+    for (var i=abbrev.length-1; i>=0; i--) {
+
+        // Convert array index to "1000", "1000000", etc
+        var size = Math.pow(10,(i+1)*3);
+
+        // If the number is bigger or equal do the abbreviation
+        if(size <= number) {
+             // Here, we multiply by decPlaces, round, and then divide by decPlaces.
+             // This gives us nice rounding to a particular decimal place.
+             number = Math.round(number*decPlaces/size)/decPlaces;
+
+             // Handle special case where we round up to the next abbreviation
+             if((number == 1000) && (i < abbrev.length - 1)) {
+                 number = 1;
+                 i++;
+             }
+
+             // Add the letter for the abbreviation
+             number += abbrev[i];
+
+             // We are done... stop
+             break;
+        }
+    }
+
+    return number;
+}
 
 $(window).scroll(function(){
   // This is then function used to detect if the element is scrolled into view
@@ -389,11 +423,11 @@ function getChart(label,like,comment) {
 
 
 function getChart2(counts) {
-    console.log(counts)
+//    console.log(counts)
     label=[]
     total=[]
     counts.sort(function (a, b) {
-        return a.count>b.count;
+        return a.count-b.count;
     });
     var j=0;
     for(var i=counts.length-1;i>=0&&j<10;i--){
@@ -715,22 +749,23 @@ function calcaulateAverage(data) {
 //function for conversion of followers, views, likes and comments into M and K
 
 function conversion(data) {
-    var value;
-    if(data>999999){
+//    var value;
+//    if(data>999999){
+//
+//        data=data/1000000;
+//        var m = Math.round(data * 100) / 100;
+//        value = m+'M';
+//    }
+//    else if(data>9999){
+//        data=data/10000;
+//        var m = Math.round(data * 100) / 100;
+//        value=m+'K';
+//    }
+//    else{
+//        value=data
+//    }
 
-        data=data/1000000;
-        var m = Math.round(data * 100) / 100;
-        value = m+'M';
-    }
-    else if(data>9999){
-        data=data/10000;
-        var m = Math.round(data * 100) / 100;
-        value=m+'K';
-    }
-    else{
-        value=data
-    }
-    return value;
+    return abbrNum(data,2);
 }
 
 //-------------------------------------------------
