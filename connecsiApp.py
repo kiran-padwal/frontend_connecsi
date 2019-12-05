4dec9161226b5013515ff8a0b7d03be08a3291d7
kiranPadwal <padwalkiran1985@gmail.com>
2019-12-05 09:46:50 +0100

    elastic search frontend

--- connecsiApp.py
+++ connecsiApp.py
@@ -7417,4 +7417,4 @@ def changingAlertNotification():
 
 if __name__ == '__main__':
     # connecsiApp.secret_key = 'connecsiSecretKey'
-    connecsiApp.run(debug=True,host='127.0.0.1',port=8090,threaded=True)
+    connecsiApp.run(debug=True,host='0.0.0.0',port=8090,threaded=True)
